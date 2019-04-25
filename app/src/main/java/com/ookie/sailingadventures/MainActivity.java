40d5d0b913bf606e9f18c608cbe6ea2c58070ac5
Ookie Ebron <ookieebron@gmail.com>
2019-04-25 15:48:34 -0400

    Initial commit

---
+++ app/src/main/java/com/ookie/sailingadventures/MainActivity.java
@@ -0,0 +1,46 @@
+package com.ookie.sailingadventures;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView reservation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        reservation = findViewById(R.id.txtReservation);
+        Button button = findViewById(R.id.btnDate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, d,c.get(Calendar.YEAR),
+                        c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    Calendar c = Calendar.getInstance();
+    DateFormat fmtDate = DateFormat.getDateInstance();
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            c.set(Calendar.YEAR, year);
+            c.set(Calendar.MONTH, monthOfYear);
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            reservation.setText("Your reservation is set for  " + fmtDate.format(c.getTime()));
+        }
+    };
+
+
+}
